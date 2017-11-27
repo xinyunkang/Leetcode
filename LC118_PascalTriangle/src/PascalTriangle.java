3afdfb535ce8f8edc59289bc4e0b8b19fbf3ca1e
xinyunkang <xinyunkang@gmail.com>
2017-11-26 20:36:42 -0600

    118

---
+++ LC118_PascalTriangle/src/PascalTriangle.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+	public static List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> rows= new ArrayList<List<Integer>>();
+		List<Integer> row = new ArrayList<Integer>();
+		for(int i=0;i<numRows;i++) {
+			row.add(0,1);
+			for(int j=1;j<row.size()-1;j++) {  //j<row.size()-1, NOT row.size()
+				row.set(j, row.get(j)+row.get(j+1));
+			}
+			rows.add(row);
+			//rows.add(new ArrayList<Integer>(row));
+		}
+		return rows;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		generate(2);
+	}
+
+}
