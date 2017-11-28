842de5bf834d3f1cc900ad0a629180b2e6fb83eb
xinyunkang <xinyunkang@gmail.com>
2017-11-28 16:44:26 -0600

    237-2

--- LC237_DeleteNodeInLinkedList/src/DeleteNodeInLinkedList.java
+++ LC237_DeleteNodeInLinkedList/src/DeleteNodeInLinkedList.java
@@ -1,8 +1,14 @@
 
 public class DeleteNodeInLinkedList {
-public void deleteNode(ListNode node) {
-        
-    }
+	public void deleteNode(ListNode node) {
+		// We can't really delete the node, but we can kinda achieve the same effect by
+		// instead removing the next node after copying its data into the node that we
+		// were asked to delete.
+		node.val=node.next.val;
+		node.next=node.next.next;
+
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
