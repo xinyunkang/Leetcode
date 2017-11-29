e27e27fb5d0fa7c434cb168237798cb9a30421e6
xinyunkang <xinyunkang@gmail.com>
2017-11-29 11:09:45 -0600

    100

---
+++ LC100_SameTree/src/SameTree.java
@@ -0,0 +1,22 @@
+
+public class SameTree {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		}
+		// could be short to p==null||q==null instead of
+		// p==null&&q!=null||p!=null&&q==null
+		if (p == null || q == null || p.val != q.val) {
+			return false;
+		}
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	}
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
