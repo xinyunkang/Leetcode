b508950ef4d7873795b03e7d293b1018a04d6ccc
xinyunkang <xinyunkang@gmail.com>
2017-12-06 17:48:20 -0600

    215

---
+++ LC215_KthLargestElementinanArray/src/KthLargestElementinanArray.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class KthLargestElementinanArray {
+	public int findKthLargest(int[] nums, int k) {
+		Arrays.sort(nums);
+		return nums[nums.length - k];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
