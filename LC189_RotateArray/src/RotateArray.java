a4b651af88489873a1ff7e514d14c5742e762a22
xinyunkang <xinyunkang@gmail.com>
2018-01-28 11:20:39 -0600

    189

--- LC189_RotateArray/src/RotateArray.java
+++ LC189_RotateArray/src/RotateArray.java
@@ -1,29 +1,46 @@
 
 public class RotateArray {
-	public static void rotate(int[] nums, int k) {
-//		k=k%nums.length;
-//		int[] nums2 = new int[nums.length * 2];
+
+//  time O(n)   space O(n)    
+//  public void rotate(int[] nums, int k) {
+//     int[] temp = new int[nums.length];
 //		for (int i = 0; i < nums.length; i++) {
-//			nums2[i] = nums[i];
-//			nums2[i + nums.length] = nums[i];
+//			//the number at index i in the original array is placed at the index (i+k)
+//         temp[(i+k)%nums.length]=nums[i]; 
 //		}
-//
 //		for (int i = 0; i < nums.length; i++) {
-//			nums[i] = nums2[i + nums.length - k];
+//			nums[i]=temp[i];
 //		}
-		
-		int[] temp = new int[nums.length];
-		for (int i = 0; i < nums.length; i++) {
-			temp[(i+k)%nums.length]=nums[i]; //the number at index i in the original array is placed at the index (i+k)
-		}
-		for (int i = 0; i < nums.length; i++) {
-			nums[i]=temp[i];
-		}
-	}
+    
+    
+// }
+
+
+public void rotate(int[] nums, int k) {
+    //Time O(n), Space O(1)
+    /*
+    example: 1234567           
+    */
+    k%=nums.length;
+    reverse(nums,0,nums.length-1); //7654321
+    reverse(nums,0,k-1); //5674321
+    reverse(nums,k,nums.length-1); //5671234
+    
+}
+
+public void reverse(int[] nums, int i, int j){
+    while(i<j){
+    int temp=nums[i];
+    nums[i++]=nums[j];
+    nums[j--]=temp;
+    }
+
+}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		rotate(new int[] { 1, 2 }, 3);
+		//rotate(new int[] { 1, 2 }, 3);
 
 	}
 
